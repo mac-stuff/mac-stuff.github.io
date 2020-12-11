efe091ebacd0483a8b2bfa40da19b31c30985770
mac-stuff <62563932+mac-stuff@users.noreply.github.com>
2020-12-11 18:08:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://morning-castle-96594.herokuapp.com/v1/task/getTasks';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const trelloApiRoot = 'https://morning-castle-96594.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
