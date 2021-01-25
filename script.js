8ca2bc9acfb033ee56de1d2a1d7204ce3c9a9e46
mac-stuff <62563932+mac-stuff@users.noreply.github.com>
2021-01-25 17:01:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://immense-hamlet-99859.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://immense-hamlet-99859.herokuapp.com/v1/trello/';
+   //const trelloApiRoot = 'https://immense-hamlet-99859.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
